34a2712993979fbbb351541c242e6d04c23395ad
Andrea Alberoni <andreaalberoni@MacBook-Pro-di-Andrea.local>
2026-05-10 11:22:26 +0200

    Aggiungi Vercel Web Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import { ThemeProvider } from '@/contexts/ThemeContext'
 import { AppShell } from '@/components/layout/AppShell'
 import Home from '@/pages/Home'
@@ -9,18 +10,21 @@ import Level4 from '@/pages/Level4'
 
 export default function App() {
   return (
-    <ThemeProvider>
-    <BrowserRouter>
-      <AppShell>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/level1" element={<Level1 />} />
-          <Route path="/level2" element={<Level2 />} />
-          <Route path="/level3" element={<Level3 />} />
-          <Route path="/level4" element={<Level4 />} />
-        </Routes>
-      </AppShell>
-    </BrowserRouter>
-    </ThemeProvider>
+    <>
+      <ThemeProvider>
+        <BrowserRouter>
+          <AppShell>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/level1" element={<Level1 />} />
+              <Route path="/level2" element={<Level2 />} />
+              <Route path="/level3" element={<Level3 />} />
+              <Route path="/level4" element={<Level4 />} />
+            </Routes>
+          </AppShell>
+        </BrowserRouter>
+      </ThemeProvider>
+      <Analytics />
+    </>
   )
 }
